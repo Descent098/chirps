78e4963d20b9b1758055caa0e8d4cd6cbdc06a18
Kieran Wood <kieran@canadiancoding.ca>
2024-07-23 23:16:18 -0600

    03-4: Changed /chirps routes to use blade views

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
-        return Response("<h1>Hello World</h1>");
+        return view("chirps.index");
     }
 
     /**
